f747c8bebb17e7a3df6e62e1065be5dda3f58285
Tugay <tugayjabbarov@gmail.com>
2025-03-20 16:05:06 +0400

    configuration

--- store/models.py
+++ store/models.py
@@ -10,3 +10,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404,
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+from .models import Product
+from .forms import ProductForm
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Qeydiyyatınız uğurla tamamlandı!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'store/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('product_list')
+        else:
+            messages.error(request, 'Yanlış istifadəçi adı və ya şifrə!')
+    return render(request, 'store/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('product_list')
+
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'store/product_form.html', {'form': form})
